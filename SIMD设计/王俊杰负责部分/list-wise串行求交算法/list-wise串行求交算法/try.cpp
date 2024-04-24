49b64791657fa64de315117f71f1d44f88fcfd33
86176 <2694841754@qq.com>
2024-04-24 20:26:26 +0800

    数据读取

---
+++ SIMD设计/王俊杰负责部分/list-wise串行求交算法/list-wise串行求交算法/try.cpp
@@ -0,0 +1,92 @@
+﻿#include <fstream>
+#include <vector>
+#include <istream>
+#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
+int main()
+{
+    // 打开二进制文件 "ExpIndex"
+    ifstream indexFile("ExpIndex", ios::binary);
+    // 创建一个二维向量 indexData 来存储数据
+    vector<vector<unsigned int>> indexData;
+
+    // 如果文件成功打开
+    if (indexFile.is_open())
+    {
+        // 循环读取文件直到文件末尾
+        while (!indexFile.eof()) {
+            // 读取数组的长度
+            unsigned int arrayLength;
+            indexFile.read(reinterpret_cast<char*>(&arrayLength), sizeof(arrayLength));
+
+            // 创建一个长度为 arrayLength 的一维向量 arrayData
+            vector<unsigned int> arrayData(arrayLength);
+            // 循环读取数组的每个元素
+            for (unsigned int i = 0; i < arrayLength; ++i) {
+                // 读取元素的值
+                unsigned int value;
+                indexFile.read(reinterpret_cast<char*>(&value), sizeof(value));
+                // 将元素的值存入 arrayData
+                arrayData[i] = value;
+            }
+
+            // 将一维向量 arrayData 存入二维向量 indexData
+            indexData.push_back(arrayData);
+        }
+
+        // 关闭文件
+        indexFile.close();
+
+
+
+
+    }
+    // 打开查询文件 "ExpQuery"
+    ifstream queryFile("ExpQuery");
+    // 创建一个三维向量 queryData 来存储查询结果
+    vector<vector<vector<unsigned int>>> queryData;
+
+    // 如果文件成功打开
+    if (queryFile.is_open())
+    {
+        // 循环读取文件直到文件末尾
+        string line;
+        while (getline(queryFile, line)) {
+            // 创建一个二维向量 queryResult 来存储查询结果
+            vector<vector<unsigned int>> queryResult;
+
+            // 将行中的每个数字转换为索引文件的数组下标，并查询对应的向量
+            stringstream ss(line);
+            unsigned int index;
+
+            while (ss >> index) {
+                // 查询对应的向量并将其存入 queryResultItem
+                queryResult.push_back(indexData[index]);
+            }
+
+            // 将一维向量 queryResult 存入二维向量 queryData
+            queryData.push_back(queryResult);
+        }
+
+        // 关闭文件
+        queryFile.close();
+    }
+
+    // 在这里可以进行后续的求交操作
+    // ...
+
+    // 输出查询结果的前10项
+    int count = 0;
+    for (const auto& result : queryData) {
+        for (const auto& value : result) {
+            cout << value[0] << " " << value[1] << " " << value[2] << " " << value[3] << " " << value[4] << " " << value[5] << "......" << endl;
+        }
+        cout << "----------------------" << endl;
+        count++;
+        if (count == 10)
+            break;
+    }
+
+}
